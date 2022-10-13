4c582f950b03006a729b8a5dca088b8572fa62ea
venkateswararao10 <104445024+venkateswararao10@users.noreply.github.com>
2022-10-13 11:11:45 +0530

    Add files via upload

---
+++ Ex1aDisplayDefaultvalueOfprimitiveDatatype.java
@@ -0,0 +1,14 @@
+class Ex1aDisplayDefaultvalueOfprimitiveDatatype{
+   static int i;
+   static float j;
+   static boolean b;
+   static char c;
+   static byte by;
+   static short s;
+   static double d;
+   static long l;
+    public static void main(String[] args){
+    System.out.println(i+" "+j+" "+c+" "+b+" "+by+" "+s+" "+d+" "+l);
+    
+    }
+}
