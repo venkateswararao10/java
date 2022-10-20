9eaf02c36a9eda6e3cf3b25e169d0f20000d24e6
venkateswararao10 <104445024+venkateswararao10@users.noreply.github.com>
2022-10-20 11:01:16 +0530

    Add files via upload

---
+++ Ex3aimplementclassmechanism.java
@@ -0,0 +1,13 @@
+public class Ex3aimplementclassmechanism {
+    static void method(){
+        System.out.println("method");
+  }
+    void metd(){
+        System.out.println("metd");
+    }
+    public static void main(String[] args){
+        method();
+        Ex3aimplementclassmechanism cla= new Ex3aimplementclassmechanism();
+        cla.metd();
+    }
+}
